49bf657b3b7d8003db2d0ab5d13aca899af0021f
Justin Bak (╯°□°)╯︵ ┻━┻ <justin.bak@onxmaps.com>
2026-03-09 09:43:58 -0600

    read file 8 bytes at a time

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	msgFile, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer msgFile.Close()
+
+	buf := make([]byte, 8)
+	for {
+		n, err := msgFile.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Fprintf(os.Stderr, "error reading: %s\n", err)
+			os.Exit(1)
+		}
+		str := string(buf[:n])
+		fmt.Printf("read: %s\n", str)
+	}
 }
